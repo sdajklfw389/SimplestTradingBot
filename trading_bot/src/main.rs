c5d1fc25f1d18863c38d717196596aa672759a75
jls <mrbillieee@gmail.com>
2023-10-29 01:43:18 +0200

    clean up code

--- trading_bot/src/main.rs
+++ trading_bot/src/main.rs
@@ -28,23 +28,42 @@ struct TickerResponse {
     price: String,
 }
 
-struct Name
-{
-    Name(base_url: &str);
+static mut APIKEY: String = String::new();
+static mut PRVKEY: String = String::new();
+static BASEURL: &'static str = "https://testnet.binance.vision/api/v3/account";
 
-    join_query_parameters();
-    place_order();
-    
-    str api_key;
-    str pri_key;
-    str base_url = "https://testnet.binance.vision/api/v3";
+fn initialize()
+{
+    let api_key_file = "./APIKey.pem";
+    let prv_key_file = "../test-prv-key.pem";
+
+    match fs::read_to_string(api_key_file) {
+        Ok(content) => {
+            unsafe{
+                APIKEY = content;
+            }
+            println!("Read success\n");
+        }
+        Err(error) => {
+            eprintln!("Error reading file: {}", error);
+        }
+    }
+
+    match fs::read_to_string(prv_key_file) {
+        Ok(content) => {
+            unsafe{
+                PRVKEY = content;
+            }
+            println!("Read success\n");
+        }
+        Err(error) => {
+            eprintln!("Error reading file: {}", error);
+        }
+    }  
 }
 
-/*
-* ret: query_string: joined parameters + '&' + timestamp
-*/
-(str, str) join_query_parameters(url_path: &str, parameters: &HashMap<&str, String>)
-{
+
+async fn place_order() -> Result<OrderResponse, Box<dyn std::error::Error>> { 
     let timestamp = chrono::Utc::now().timestamp_millis();
 
     // Create the query parameters for the sell order
@@ -52,39 +71,35 @@ struct Name
     query_params.insert("timestamp", timestamp.to_string());
 
     // Generate the query string
-    let query_string = parameters.iter()
+    let query_string = query_params.iter()
         .map(|(k, v)| format!("{}={}", k, v))
         .collect::<Vec<String>>()
         .join("&");
-}
 
-async fn place_order(method: &str, url_path: &str, query_string: &str, parameters: &HashMap<&str, String>) -> Result<OrderResponse, Box<dyn std::error::Error>> {
-    
-    let (url, query_string) = join_query_parameters(url_path, parameters);
     // First step: create a SHA-256 hash of the message.
     let mut hasher = Sha256::new();
     hasher.update(query_string);
     let hashed_message = hasher.finalize();
 
     // Sign the hashed message.
-    let private_key = RsaPrivateKey::from_pkcs8_pem(&secret_key).expect("invalid pem");
+    let private_key = unsafe{ RsaPrivateKey::from_pkcs8_pem(&PRVKEY).expect("invalid pem")};
     let padding = Pkcs1v15Sign::new::<rsa::sha2::Sha256>();
     let signature = private_key.sign(padding, &hashed_message).expect("failed to encrypt");
 
     // Create the request headers
     let mut headers = HeaderMap::new();
-    headers.insert("X-MBX-APIKEY", HeaderValue::from_str(api_key).unwrap());
+    unsafe{
+        headers.insert("X-MBX-APIKEY", HeaderValue::from_str(APIKEY.as_str()).unwrap());
+    }
 
     // Build the request
     let client = reqwest::Client::new();
     // This is just retrieve testnet account info
-    // let url = format!("{}?timestamp={}&signature={}",
-    //     "https://testnet.binance.vision/api/v3/account",
-    //     timestamp,
-    //     base64::encode(&signature)
-    // );
-
-    let url = base_url + url_path + query_string + signature;
+    let url = format!("{}?timestamp={}&signature={}",
+        BASEURL,
+        timestamp,
+        base64::encode(&signature)
+    );
 
     let response = client.get(&url)
         .headers(headers)
@@ -105,6 +120,8 @@ async fn place_order(method: &str, url_path: &str, query_string: &str, parameter
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    initialize();
+
     let client = reqwest::Client::new();
 
     let url_account_info = "https://testnet.binance.vision/api/v3/account";
@@ -121,34 +138,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         {
             println!("ETH to USD price exceeds 1830, sell");
     
-            let api_key_file = "./APIKey.pem";
-            let prv_key_file = "../test-prv-key.pem";
-            let mut api_key_content = String::new();
-            let mut prv_key_content = String::new();
             let symbol = "ETHUSD";
             let quantity = "1.5";
     
-            match fs::read_to_string(api_key_file) {
-                Ok(content) => {
-                    api_key_content = content;
-                    println!("Read success\n");
-                }
-                Err(error) => {
-                    eprintln!("Error reading file: {}", error);
-                }
-            }
-    
-            match fs::read_to_string(prv_key_file) {
-                Ok(content) => {
-                    prv_key_content = content;
-                    println!("Read success\n");
-                }
-                Err(error) => {
-                    eprintln!("Error reading file: {}", error);
-                }
-            }
         
-            let response = place_order(api_key_content.as_str(), prv_key_content.as_str(), url, query_string).await;
+            let response = place_order().await;
             match response {
                 Ok(order) => println!("Sell order placed successfully. Order ID: {}", order.order_id),
                 Err(e) => eprintln!("Error placing sell order: {}", e),
